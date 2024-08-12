a2dd96c79b07557351e07baa0855a95121c675fe
hushtar <HUXTAR.EZ@gmail.com>
2024-08-12 20:15:14 +0200

    updated

--- src/test/com/example/demo/calculator/CalculatorTest.java
+++ src/test/com/example/demo/calculator/CalculatorTest.java
@@ -1,7 +1,6 @@
 package com.example.demo.calculator;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,6 +15,7 @@ class CalculatorTest {
     void testIntegerDivision_WhenSixIsDividedByThree_ShouldReturnTwo() {
         int result =calculator.IntegerDivision(6,3);
         assertEquals(2,result,"Hello Testing");
+        System.out.println("1");
     }
 
     @DisplayName("Subtraction")
@@ -23,11 +23,35 @@ class CalculatorTest {
     void testIntegerSubtraction_WhenFiveIsSubtractedByOne_ShouldReturnFour() {
         int result=calculator.integerSubtraction(5,1);
         assertEquals(4,result);
+        System.out.println("2");
     }
 
+    @Disabled("ps : this method needs update")
     @DisplayName("RandomError")
-    @Test
+    @Test    // add @Disabled annotation or comment out @Test annotation for not executing
     void testRandomError_WhenAppRunning() {
         fail();
+        System.out.println("3");
+    }
+
+
+    @BeforeAll //must be static
+    static void thisIsBeforeAll(){
+        System.out.println("Before All annotation");
+    }
+
+    @AfterAll //must be static
+    static void thisIsAfterAll(){
+        System.out.println("After All annotation");
+    }
+
+    @BeforeEach
+    void thisIsBeforeEach(){
+        System.out.println("Before Each annotation");
+    }
+
+    @AfterEach
+    void thisIsAfterEach(){
+        System.out.println("After Each annotation");
     }
 }
