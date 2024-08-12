25e5377f817b8fbe815e472246fb9eaa7e5d5d72
hushtar <HUXTAR.EZ@gmail.com>
2024-08-12 15:37:11 +0200

    initial commit

---
+++ src/main/java/com/example/demo/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package com.example.demo.calculator;
+
+public class Calculator {
+    public int IntegerDivision(int a, int b) {
+        return a / b;
+    }
+
+    public int integerSubtraction(int minuend, int subtrahend) {
+        return minuend - subtrahend;
+    }
+}
